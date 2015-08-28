c3efb08487097317dd5c35da3f2f85f83d52eea2
Neil <github@moley.org.uk>
2015-08-28 12:28:24 +0100

    separate tests

--- test.js
+++ test.js
@@ -22,72 +22,86 @@ var getMongooseVersion = function() {
 
 
 tap.test("mongoose with cls", function (t) {
-
+    
     var mongooseVersion = getMongooseVersion();
-
+    
     //connect mongoose to some mongo instance
     mongoose.connect('mongodb://localhost/mongoose-cls-test', function(err) {
-
+        
         //define a mongoose Model
         var TestModel = mongoose.model('test_model', mongoose.Schema({value: String}));
-
-        //initialise a cls, apply the cls-mongoose shim then .run() the rest of the test within it
-        clsns.run(function() {
-            clsns.set("nsvalue", "set");
-
-            t.equals(clsns.get("nsvalue"), "set");
-
-            setTimeout(function() {
-                //prove that the cls-stored value is available across setTimeout()
+        
+        
+        t.test("setTimeout - mongoose " + mongooseVersion, function(tt) {
+            //initialise a cls, apply the cls-mongoose shim then .run() the rest of the test within it
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+    
                 t.equals(clsns.get("nsvalue"), "set");
-
-
-                TestModel.aggregate({$match: {"nonexistent_field": "nonexistent_value"}}
-                                   ).exec(function(err, aggregateResult) {
-                    //check that the cls-stored value is still available
-                    t.equals(clsns.get("nsvalue"), "set");
-                    if (clsns.get("nsvalue") !== "set") t.bailout("cls value empty after TestModel.aggregate() - mongooseVersion: " + mongooseVersion);
-
-
-                    //do Model.find
-                    TestModel.find({}, function(err, findResult) {
-                        //check that the cls-stored value is still available
-                        t.equals(clsns.get("nsvalue"), "set");
-                        if (clsns.get("nsvalue") !== "set") t.bailout("cls value empty after TestModel.find() - mongooseVersion: " + mongooseVersion);   //FAILS HERE ON MONGOOSE 4.0 or 4.1 but 3.8 or 3.9 are ok
-
-
-                        //do Model.update
-                        TestModel.update(
-                            {"nonexistent_field": "nonexistent_value"},
-                            {$set: {value: "modified entry"}}, function(err, updateResult) {
-
-                            //check that the cls-stored value is still available
-                            t.equals(clsns.get("nsvalue"), "set");
-                            if (clsns.get("nsvalue") !== "set") t.bailout("cls value empty after TestModel.update() - mongooseVersion: " + mongooseVersion);       //FAILS HERE ON MONGOOSE 3.8 or 3.9
-
-
-                            //do Model.distinct
-                            TestModel.distinct('doesntExist', function (err, findResult) {
-                                t.equals(clsns.get("nsvalue"), "set");
-                                if (clsns.get("nsvalue") !== "set") t.bailout("cls value empty after TestModel.distinct() - mongooseVersion: " + mongooseVersion);
-
-
-                                TestModel.count({}, function (err, findResult) {
-                                    t.equals(clsns.get("nsvalue"), "set");
-                                    if (clsns.get("nsvalue") !== "set") t.bailout("cls value empty after TestModel.count() - mongooseVersion: " + mongooseVersion);
-
-                                    t.end();
-                                });
-                            });
-                        });
-                    });
-                });
-
-            }, 500);
+    
+                setTimeout(function() {
+                    //prove that the cls-stored value is available across setTimeout()
+                    tt.equals(clsns.get("nsvalue"), "set");
+    
+                    tt.end();
+                }, 500);
+            });
         });
-
-
-    });
+        
+        t.test("find - mongoose " + mongooseVersion, function(tt) {
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+                TestModel.find({}, function (err, findResult) {
+                    tt.equals(clsns.get("nsvalue"), "set");
+                    tt.end();
+                });
+            });
+        });
+        
+        t.test("update - mongoose " + mongooseVersion, function(tt) {
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+                TestModel.update( {"nonexistent_field": "nonexistent_value"}, {$set: {value: "modified entry"}}, function(err, updateResult) {
+                    tt.equals(clsns.get("nsvalue"), "set");
+                    tt.end();
+                });
+            });
+        });
+        
+        t.test("distinct - mongoose " + mongooseVersion, function(tt) {
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+                TestModel.distinct('doesntExist', function (err, findResult) {
+                    tt.equals(clsns.get("nsvalue"), "set");
+                    tt.end();
+                });
+            });
+        });
+        
+        t.test("count - mongoose " + mongooseVersion, function(tt) {
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+                TestModel.count({}, function (err, findResult) {
+                    tt.equals(clsns.get("nsvalue"), "set");
+                    tt.end();
+                });
+            });
+        });
+        
+        t.test("aggregate - mongoose " + mongooseVersion, function(tt) {
+            clsns.run(function() {
+                clsns.set("nsvalue", "set");
+                TestModel.aggregate({$match: {"nonexistent_field": "nonexistent_value"}})
+                    .exec(function(err, aggregateResult) {
+                        tt.equals(clsns.get("nsvalue"), "set");
+                        tt.end();
+                    }
+                );
+            });
+        });
+        
+        t.end();
+    });  //mongoose.connect()
 
 });
 
