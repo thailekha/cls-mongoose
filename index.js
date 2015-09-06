214bfbcbde6f6bc9df18dc5815bb4735e82a4301
Neil <github@moley.org.uk>
2015-09-07 00:03:56 +0100

    comment back in 'on', which allows aggregate on mongoose 3.8; lower mongoose version to 3.8

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ module.exports = function patchMPromise(ns) {
   if (typeof ns.bind !== 'function') {
     throw new TypeError("must include namespace to patch Mongoose against");
   }
-/*
+  
   shimmer.wrap(mongoose.Mongoose.prototype.Promise.prototype, 'on', function (original) {
     return function(event, callback) {
       callback = ns.bind(callback);
       return original.call(this, event, callback);
     };
   });
-*/
+  
   shimmer.wrap(mongoose.Mongoose.prototype.Query.prototype, 'exec', function (original) {
     return function(op, callback) {
       if (typeof op == 'function') op = ns.bind(op);
